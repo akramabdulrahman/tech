7e9cc78bcd7ff814354bcc1a6add5e2b1ca3b9db
akramabdulrahman <blackthrone91@gmail.com>
2017-05-18 15:36:07 +0300

    =added text input

--- src/App.js
+++ src/App.js
@@ -14,42 +14,53 @@ var ColrComponent = React.createClass({
             }
         }
     },
-    getData:function(context){
-       return  $.get('http://www.colr.org/json/color/random')
-            .then( function(data) {
+    getData: function (context) {
+        return $.get('http://www.colr.org/json/color/random')
+            .then(function (data) {
                 context.setState({color: JSON.parse(data).colors[0].hex});
             });
     },
     componentDidMount: function () {
         this.getData(this);
     },
-    componentWillMount:function(){
+    componentWillMount: function () {
 
     },
-    clicked :function () {
-        if(!'000'.includes(this.state.color)){
-            this.setState({color:'000'});
-        }else{
+    clicked: function () {
+        if (!'000'.includes(this.state.color)) {
+            this.setState({color: '000'});
+        } else {
             this.getData(this);
         }
     },
     render: function () {
-        return <h1 onClick={this.clicked} style={{color: '#'+this.state.getColor()}}> {this.props.name}</h1>;
+        return <h1 onClick={this.clicked} style={{color: '#' + this.state.getColor()}}> {this.props.name}</h1>;
     }
 });
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            headerText: 'Welcome to React'
+        };
+        this.changeHeaderText = this.changeHeaderText.bind(this);
 
+    }
+
+    changeHeaderText(evt) {
+        this.setState({headerText: evt.target.value})
+    }
 
     render() {
         return (
             <div className="App">
                 <div className="App-header">
                     <img src={logo} className="App-logo" alt="logo"/>
-                    <ColrComponent name="Welcome to React"/>
+                    <ColrComponent name={this.state.headerText}/>
                 </div>
                 <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
+                    To get started, edit <input type="text" onChange={this.changeHeaderText} value={this.state.headerText}/> and save to reload.
                 </p>
             </div>
         );
